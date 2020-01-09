b6c6d6d58ab8c30292e1d17987e8aea29acf344a
trungdoan1707 <duytrung177@gmail.com>
2020-01-09 18:59:09 +0700

    create project

---
+++ ProjectSem4/src/main/java/com/softech/projectsem4/projectsem4/ProjectSem4Application.java
@@ -0,0 +1,13 @@
+package com.softech.projectsem4.projectsem4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSem4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSem4Application.class, args);
+    }
+
+}
---
+++ ProjectSem4/src/test/java/com/softech/projectsem4/projectsem4/ProjectSem4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softech.projectsem4.projectsem4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSem4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
